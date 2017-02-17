ff37d91a2abae6c6498ef98a0e77424c18d39d8e
T3 <T3@10.54.3.101>
2017-02-16 19:04:03 -0500

    //Initial draft of DriveTrain.java

--- src/org/usfirst/frc/team68/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team68/robot/subsystems/DriveTrain.java
@@ -1,38 +1,120 @@
 
 package org.usfirst.frc.team68.robot.subsystems;
 
+import org.usfirst.frc.team68.robot.RobotMap;
+import org.usfirst.frc.team68.robot.commands.DriveWithXboxJoysticks;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+//import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
     
-	// Declare instance variables here
+	private CANTalon leftFront;
+	private CANTalon leftRear;
+	private CANTalon rightFront;
+	private CANTalon rightRear;
+	private RobotDrive drive;
+	//private DoubleSolenoid driveShifter;
+	private boolean gear = false;
+	private boolean mid = false;
+	private static boolean useSquaredInputs = true;
+	private int absolutePositionLeftRear;
+	private int absolutePositionRightRear;
+	private boolean isPercentVbus = true;
 
-    
-    // Declare Class variables here
-    private static DriveTrain driveTrain;
-
-    
-    public static DriveTrain getDriveTrain() {
-    	if (driveTrain == null) {
-    		driveTrain = new DriveTrain();
-    	}
-    	return driveTrain;
-    }
-    
+	private static DriveTrain driveTrain;
+	
+	public static DriveTrain getDrive () {
+		if (driveTrain == null) {
+			driveTrain = new DriveTrain();
+		}
+		return driveTrain;
+	}
     // Constructor
-    private DriveTrain() {
+	private DriveTrain() {
+		
+		// Instantiate Drive Motors
+		leftRear = new CANTalon(RobotMap.DRIVE_LEFT_REAR);
+		rightRear = new CANTalon(RobotMap.DRIVE_RIGHT_REAR);
+		
+		leftFront = new CANTalon(RobotMap.DRIVE_LEFT_FRONT);
+		leftFront.changeControlMode(CANTalon.TalonControlMode.Follower);
+    	leftFront.set(leftRear.getDeviceID());
+    	
+		rightFront = new CANTalon(RobotMap.DRIVE_RIGHT_FRONT);
+		rightFront.changeControlMode(CANTalon.TalonControlMode.Follower);
+    	rightFront.set(rightRear.getDeviceID());
+    	
+    	leftFront.enableBrakeMode(false);
+    	leftRear.enableBrakeMode(false);
+    	rightFront.enableBrakeMode(false);
+    	leftFront.enableBrakeMode(false);
 
-    }
-    
+
+		drive = new RobotDrive(leftRear, rightRear);
+		
+
+		drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+		drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+		
+		
+		drive.setSafetyEnabled(true);
+	
+		//	driveShifter = new DoubleSolenoid(RobotMap.DRIVE_SHIFTER_PCM_A, RobotMap.DRIVE_SHIFTER_PCM_B);
+		//		this.setShifterLow();
+	}
+
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-    	// setDefaultCommand(new Command() );
+       
+    	setDefaultCommand(new DriveWithXboxJoysticks());
     }
     
-    // Create custom methods here
-
+    public void tankDrive(double leftSpeed, double rightSpeed) {
+    	
+    	leftSpeed*=RobotMap.DRIVE_MULT;
+   		rightSpeed*=RobotMap.DRIVE_MULT;
+    	
+    	drive.tankDrive(leftSpeed, rightSpeed, useSquaredInputs);
+    }
+    
+    public void setPower(double power){
+    	leftRear.set(power*-1);
+    	rightRear.set(power);
+    }
+    public void setPowers(double powerL, double powerR){
+    	leftRear.set(powerL*-1);
+    	rightRear.set(powerR);
+    }
+    
+    /*public void setShifterHigh() {
+    	driveShifter.set(Value.kForward);
+    	gear = true;
+    	mid = true;
+    }
+    
+    public void setShifterLow() {
+    	driveShifter.set(Value.kReverse);
+    	gear = false;
+    }
+    
+    public void shift() {
+    	if(this.getShifter() == Value.kForward){
+    	  	this.setShifterLow();
+    	} else {
+    		this.setShifterHigh();
+    	}
+    }
+    
+    public DoubleSolenoid.Value getShifter() {
+    	return driveShifter.get();
+    
+    }*/
 }
-
